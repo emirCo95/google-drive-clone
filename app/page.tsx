4adb49a11d0fd4522e3f61e2fc631382e7499872
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-13 15:38:36 -0500

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex-center">
+      <h1 className="text-3xl text-brand-100">StoreIt</h1>
+    </div>
+  );
+}
